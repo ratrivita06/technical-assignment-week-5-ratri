3e2b9a17c713b212b65f7adb5cea057ad86b5e65
ratrivita06 <ratrivita999@gmail.com>
2022-07-11 13:08:56 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import dht11
+import time
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    x = []
+    for i in range(5):
+        x.append(dht11.ambil())
+        time.sleep(0.2)
+    
+    print(x)
+    plt.plot(x)
+    plt.show()
+
+
+def panasDingin(suhu):
+        if suhu < 30:
+            return "masih Dingin"
+        else :
+            return "sudah Panas"
+        
+while True:
+    hm = dht11.ambil()
+    print("%0.1f*C, kondisi "%(hm) + panasDingin(hm))
